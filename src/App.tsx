6dbaf34f5f8cce98b35ca37c93227595f3243d6e
Racheal I. Ogunmodede <razylove02@gmail.com>
2026-05-10 21:18:04 +0100

    implement responsive Navbar and project infrastructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { Navbar } from "@/components/landing/Navbar";
-import { Hero } from "@/components/landing/Hero";
+import { Navbar } from "./components/landing/NavBar";
+/* import { Hero } from "@/components/landing/Hero";
 import { Features } from "@/components/landing/Features";
 import { HowItWorks } from "@/components/landing/HowItWorks";
 import { Testimonials } from "@/components/landing/Testimonials";
 import { Pricing } from "@/components/landing/Pricing";
 import { FAQ } from "@/components/landing/FAQ";
 import { Footer } from "@/components/landing/Footer";
+ */
 
-function Index() {
+export default function Index() {
   return (
     <main className="min-h-screen">
       <Navbar />
-      <Hero />
+      {/* <Hero />
       <Features />
       <HowItWorks />
       <Testimonials />
       <Pricing />
       <FAQ />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 }
